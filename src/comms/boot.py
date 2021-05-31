3ca7fb5b13f378fa257cc37d197fde50e9498370
jreamm <jaidonlybbert@gmail.com>
2021-05-31 16:22:24 -0700

    Added connection to router to boot.py

--- src/comms/boot.py
+++ src/comms/boot.py
@@ -5,5 +5,19 @@ import uos, machine
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 import webrepl
+import network
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('McChunder', 'Deeznutz')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
 webrepl.start()
 gc.collect()
